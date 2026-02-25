636a5b439df9646a58412cf1b0bc868846c1b763
H1101241014-Naomy <H1101241014@student.untan.ac.id>
2026-02-25 04:46:53 +0000

    update index2

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 function hello(nama:string, umur: number): string {
 return 'selamat datang '+nama+'  di PWL, umur '+umur+' adalah';
 }
-const mahasiswa="Andi";
+const mahasiswa="Kayla";
 const umur:number =19;
 console.log(hello(mahasiswa, umur));
