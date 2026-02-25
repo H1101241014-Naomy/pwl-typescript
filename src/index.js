9b0130b657029e9a0cd5b7adb488f1f936bee642
H1101241014-Naomy <H1101241014@student.untan.ac.id>
2026-02-25 04:43:10 +0000

    update index

---
+++ src/index.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+function hello(nama) {
+    return `selamat datang ` + nama + `  di PWL` + nama;
+}
+console.log(hello('Jon Down'));
+//# sourceMappingURL=index.js.map
---
+++ src/index.ts
@@ -0,0 +1,6 @@
+function hello(nama:string, umur: number): string {
+return 'selamat datang '+nama+'  di PWL, umur '+umur+' adalah';
+}
+const mahasiswa="Andi";
+const umur:number =19;
+console.log(hello(mahasiswa, umur));
